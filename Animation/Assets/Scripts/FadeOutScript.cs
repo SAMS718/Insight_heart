ed27d87662c4ecb0cfc4faa8c0e0237ec5b38719
Uma <136799062+Umasaisrimasina@users.noreply.github.com>
2025-06-17 15:23:00 +0530

    commit 55

--- Animation/Assets/Scripts/FadeOutScript.cs
+++ Animation/Assets/Scripts/FadeOutScript.cs
@@ -1,27 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FadeOutScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    SpriteRenderer rend;
+    private Renderer rend;
+    private Material mat;
+
     void Start()
     {
-        rend = GetComponent<SpriteRenderer>();
-        
+        rend = GetComponent<Renderer>();
+        if (rend != null)
+        {
+            mat = rend.material;
+            // Ensure the shader supports transparency
+            mat.SetFloat("_Mode", 2); // Use Transparent mode
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
+        }
     }
 
-    IEnumerator FadeIn(){
-        for(float f = 1f;f>=-0.05f;f-=0.05f){
-            Color c = rend.material.color;
-            c.a = f;
-            rend.material.color = c;
+    IEnumerator FadeIn()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            if (mat != null)
+            {
+                Color c = mat.color;
+                c.a = f;
+                mat.color = c;
+            }
             yield return new WaitForSeconds(0.05f);
         }
     }
 
-    public void startFading(){
-        StartCoroutine("Fade In");
+    public void startFading()
+    {
+        StartCoroutine(FadeIn());
     }
 }
